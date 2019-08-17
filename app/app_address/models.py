e24272ff8bac7428c7c5ffb04f2c0c72d1156958
ihor-nahuliak <ihor.nahuliak@gmail.com>
2019-08-17 18:50:37 +0200

    deduplication

--- app/app_address/models.py
+++ app/app_address/models.py
@@ -1,4 +1,4 @@
-from django.db import models
+from django.db import models, IntegrityError
 from django.contrib.auth import get_user_model
 
 
@@ -26,12 +26,109 @@ class UserAddress(models.Model):
     full_address = models.TextField(
         blank=True)
 
+    def _get_duplicates_queryset(self):
+        """Here we form a queryset
+        to find not full filled addresses.
+
+        e.g. we match these values as
+        not full filled or duplicated:
+        +------------------+------------------+
+        | old stored value | new value        |
+        +------------------+------------------+
+        | "Max"            | "Max Mustermann" |
+        | "Mustermann"     | "Max Mustermann" |
+        | "Max Mustermann" | "Max Mustermann" |
+        | ""               | "Max Mustermann" |
+        | None             | "Max Mustermann" |
+        +------------------+------------------+
+
+        :rtype: models.QuerySet
+        """
+        name__tags = (self.name or '').split(' ')
+        if self.name:
+            name__tags.append(self.name)
+
+        street_address__tags = (self.street_address or '').split(' ')
+        if self.street_address:
+            street_address__tags.append(self.street_address)
+
+        street_address2__tags = (self.street_address_line2 or '').split(' ')
+        if self.street_address_line2:
+            street_address2__tags.append(self.street_address_line2)
+
+        zip_code__tags = (self.zipcode or '').split(' ')
+        if self.zipcode:
+            zip_code__tags.append(self.zipcode)
+
+        city__tags = (self.city or '').split(' ')
+        if self.city:
+            city__tags.append(self.city)
+
+        state__tags = (self.state or '').split(' ')
+        if self.state:
+            state__tags.append(self.state)
+
+        country__tags = (self.country or '').split(' ')
+        if self.country:
+            country__tags.append(self.country)
+
+        q = self.__class__.objects.filter(
+            models.Q(
+                user_id=self.user_id
+            ) &
+            (
+                models.Q(name__in=name__tags) |
+                models.Q(name='') |
+                models.Q(name__isnull=True)
+            ) &
+            (
+                models.Q(street_address__in=street_address__tags) |
+                models.Q(street_address='') |
+                models.Q(street_address__isnull=True)
+            ) &
+            (
+                models.Q(street_address_line2__in=street_address2__tags) |
+                models.Q(street_address_line2='') |
+                models.Q(street_address_line2__isnull=True)
+            ) &
+            (
+                models.Q(zipcode__in=zip_code__tags) |
+                models.Q(zipcode='') |
+                models.Q(zipcode__isnull=True)
+            ) &
+            (
+                models.Q(city__in=city__tags) |
+                models.Q(city='') |
+                models.Q(city__isnull=True)
+            ) &
+            (
+                models.Q(state__in=state__tags) |
+                models.Q(state='') |
+                models.Q(state__isnull=True)
+            ) &
+            (
+                models.Q(country__in=country__tags) |
+                models.Q(country='') |
+                models.Q(country__isnull=True)
+            )
+        )
+        return q
+
+    def _deduplicate(self):
+        q = self._get_duplicates_queryset()
+        duplicates = list(q[:2].all())
+        if not self.pk and len(duplicates) == 1:
+            self.pk = duplicates[0].pk
+        elif duplicates:
+            raise IntegrityError('address duplicated error')
+
     def save(self, *args, **kwargs):
         # It puts None values to the full_address,
         # but I didn't change that task logic.
         streetdata = f'{self.street_address}\n{self.street_address_line2}'
         self.full_address = (f'{streetdata}\n{self.zipcode} {self.city} '
                              f'{self.state} {self.country}')
+        self._deduplicate()
         super().save(*args, **kwargs)
 
     class Meta:
--- app/tests/test_user_address_model.py
+++ app/tests/test_user_address_model.py
@@ -1,6 +1,7 @@
 import unittest
 
 import django.test
+from django.db import IntegrityError
 from django.core.management import call_command
 
 from app_address.models import UserAddress
@@ -58,6 +59,97 @@ class TestCase(django.test.TestCase):
         m.save()
         self.assertEqual(m.full_address,
                          '789 Otherstreet\nNone\nNone Giventown None NL')
+
+    def test_deduplication_leaves_just_2_unique_records(self):
+        m = UserAddress(user_id=1,
+                        name='Max',
+                        city='Giventown')
+        m.save()
+        m = UserAddress(user_id=1,
+                        name='Max Mustermann',
+                        street_address='Randomstreet',
+                        city='Giventown')
+        m.save()
+        m = UserAddress(user_id=1,
+                        name='Max Mustermann',
+                        street_address='456 Randomstreet',
+                        city='Giventown')
+        m.save()
+        m = UserAddress(user_id=1,
+                        name='Max Mustermann',
+                        street_address='789 Otherstreet',
+                        city='Giventown',
+                        country='NL')
+        m.save()
+        total_count = UserAddress.objects.count()
+
+        self.assertEqual(2, total_count)
+
+    def test_deduplication_raises_conflict_error_on_different_pk(self):
+        m = UserAddress(id=1,
+                        user_id=1,
+                        name='Max',
+                        city='Giventown')
+        m.save()
+        m = UserAddress(id=2,
+                        user_id=1,
+                        name='Max Mustermann',
+                        street_address='Randomstreet',
+                        city='Giventown')
+        with self.assertRaises(IntegrityError) as err_ctx:
+            m.save()
+
+        self.assertEqual(err_ctx.exception.args[0], 'address duplicated error')
+
+    def test_match__name_not_filled(self):
+        m1 = UserAddress(user_id=1, name='')
+        m1.save()
+        m2 = UserAddress(user_id=1, name='lorem ipsum dolor')
+        m2.save()
+
+        self.assertEqual(m2.id, m1.id)
+
+    def test_match__name_filled_left(self):
+        m1 = UserAddress(user_id=1, name='lorem')
+        m1.save()
+        m2 = UserAddress(user_id=1, name='lorem ipsum dolor')
+        m2.save()
+
+        self.assertEqual(m2.id, m1.id)
+
+    def test_match__name_filled_center(self):
+        m1 = UserAddress(user_id=1, name='ipsum')
+        m1.save()
+        m2 = UserAddress(user_id=1, name='lorem ipsum dolor')
+        m2.save()
+
+        self.assertEqual(m2.id, m1.id)
+
+    def test_match__name_filled_right(self):
+        m1 = UserAddress(user_id=1, name='dolor')
+        m1.save()
+        m2 = UserAddress(user_id=1, name='lorem ipsum dolor')
+        m2.save()
+
+        self.assertEqual(m2.id, m1.id)
+
+    def test_match__name_filled_fully_duplicated(self):
+        m1 = UserAddress(user_id=1, name='lorem ipsum dolor')
+        m1.save()
+        m2 = UserAddress(user_id=1, name='lorem ipsum dolor')
+        m2.save()
+
+        self.assertEqual(m2.id, m1.id)
+
+    # def test_match__name_filled_better_before(self):
+    #     # TODO: it takes the longest old value as a better one ???
+    #     m1 = UserAddress(user_id=1, name='lorem ipsum dolor')
+    #     m1.save()
+    #     m2 = UserAddress(user_id=1, name='lorem ipsum')
+    #     m2.save()
+    #
+    #     self.assertEqual(m2.id, m1.id)
+    #     self.assertEqual(m2.name, 'lorem ipsum dolor')
 
 
 if __name__ == '__main__':
